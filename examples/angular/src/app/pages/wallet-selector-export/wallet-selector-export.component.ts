c93274f654d93a388bd43a1f9884b88ce162e1ac
Dimitar Daskalov <dimitar.daskalov@limechain.tech>
2024-07-11 10:37:12 +0300

    fix: use constant for CONTRACT_ID instead of hardcoded string

--- examples/angular/src/app/pages/wallet-selector-export/wallet-selector-export.component.ts
+++ examples/angular/src/app/pages/wallet-selector-export/wallet-selector-export.component.ts
@@ -23,6 +23,8 @@ import { setupNearMobileWallet } from "@near-wallet-selector/near-mobile-wallet"
 import { setupLedger } from "@near-wallet-selector/ledger";
 import { setupMintbaseWallet } from "@near-wallet-selector/mintbase-wallet";
 import { setupBitteWallet } from "@near-wallet-selector/bitte-wallet";
+// eslint-disable-next-line @nx/enforce-module-boundaries
+import { CONTRACT_ID } from "examples/angular/src/constants";
 
 declare global {
   interface Window {
@@ -81,8 +83,8 @@ export class WalletSelectorExportComponent implements OnInit {
         }),
         setupRamperWallet(),
         setupNearMobileWallet(),
-        setupMintbaseWallet({ contractId: "guest-book.testnet" }),
-        setupBitteWallet({ contractId: "guest-book.testnet" }),
+        setupMintbaseWallet({ contractId: CONTRACT_ID }),
+        setupBitteWallet({ contractId: CONTRACT_ID }),
       ],
     });
     /**
